4415c1440c0e88ba53689c004a6f3c2311d6a7fc
lewis-weinberger <lweinberger@btinternet.com>
2022-04-30 23:27:12 +0100

    altered syntax highlighting
    Removed italic/bold. Changed C rules. Added basic Rust and Smalltalk rules. Added install target to Makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CC = cc
-CFLAGS = -Wall -O2
+CFLAGS = -Wall -O2 -s
 LDFLAGS =
+PREFIX ?= /usr/local
 
 OBJS = vi.o ex.o lbuf.o mot.o sbuf.o ren.o dir.o syn.o reg.o led.o \
 	uc.o term.o rset.o rstr.o regex.o cmd.o conf.o
@@ -13,5 +14,11 @@ conf.o: conf.h
 	$(CC) -c $(CFLAGS) $<
 vi: $(OBJS)
 	$(CC) -o $@ $(OBJS) $(LDFLAGS)
+install: vi
+	mkdir -p $(PREFIX)/bin
+	cp -f vi $(PREFIX)/bin/vi
+	chmod 755 $(PREFIX)/bin/vi
 clean:
 	rm -f *.o vi
+
+.PHONY: clean all install
--- conf.h
+++ conf.h
@@ -17,8 +17,18 @@ static struct filetype {
 	{"py", "\\.py$"},				/* python */
 	{"bib", "bib$"},				/* refer */
 	{"nm", "\\.nm$"},				/* neatmail */
-	{"diff", "\\.(patch|diff)$"}			/* diff */
+	{"diff", "\\.(patch|diff)$"},			/* diff */
+	{"rust", "\\.rs$"},				/* rust */
+	{"st", "\\.st$"}				/* smalltalk */
 };
+
+#define HL_TYPE 5
+#define HL_KEYWORD 4
+#define HL_COMMENT 2
+#define HL_STRING 4
+#define HL_FUNCTION 6
+#define HL_NUMBER 1
+#define HL_MISC 3
 
 /* syntax highlighting patterns */
 static struct highlight {
@@ -27,61 +37,63 @@ static struct highlight {
 	char *pat;		/* regular expression */
 	int end;		/* the group ending this pattern */
 } highlights[] = {
-	{"c", {5}, "\\<(signed|unsigned|char|short|int|long|float|double|void|struct|enum|union|typedef)\\>"},
-	{"c", {5}, "\\<(static|extern|register)\\>"},
-	{"c", {4}, "\\<(return|for|while|if|else|do|sizeof|goto|switch|case|default|break|continue)\\>"},
-	{"c", {2 | SYN_IT}, "//.*$"},
-	{"c", {2 | SYN_IT}, "/\\*([^*]|\\*+[^*/])*\\*+/"},
-	{"c", {6}, "^#[ \t]*[a-zA-Z0-9_]+"},
-	{"c", {0, SYN_BD}, "([a-zA-Z][a-zA-Z0-9_]+)\\(", 1},
-	{"c", {4}, "\"([^\"]|\\\\\")*\""},
-	{"c", {4}, "'([^\\]|\\\\.)'"},
-	{"c", {4}, "[-+]?\\<(0[xX][0-9a-fA-F]+|[0-9]+)\\>"},
+	{"c", {HL_TYPE}, "\\<(signed|unsigned|char|short|int|long|float|double|void|struct|enum|union|typedef)\\>"},
+	{"c", {HL_TYPE}, "\\<(static|extern|register)\\>"},
+	{"c", {HL_TYPE}, "[a-zA-Z][a-zA-Z0-9_]*_t"},
+	{"c", {HL_KEYWORD}, "\\<(return|for|while|if|else|do|sizeof|goto|switch|case|default|break|continue)\\>"},
+	{"c", {HL_COMMENT}, "//.*$"},
+	{"c", {HL_COMMENT}, "(/\\*[^*/]*)|([^\"/*]*\\*/)"},
+	{"c", {HL_COMMENT}, "(^[[:blank:]]\\*[^*/]*)"}, /* hack for multiline comments */
+	{"c", {HL_MISC}, "^#[ \t]*[a-zA-Z0-9_]+"},
+	{"c", {HL_FUNCTION}, "([a-zA-Z][a-zA-Z0-9_]+)\\(", 1},
+	{"c", {HL_STRING}, "\"(\\\\\"|[^\"])*\""},
+	{"c", {HL_STRING}, "'(\\\\'|[^'])*'"},
+	{"c", {HL_NUMBER}, "[-+]?\\<(0[xX][0-9a-fA-F]+|[0-9]+)\\>"},
 
-	{"roff", {4, 0, 5 | SYN_BD, 4 | SYN_BD, 5 | SYN_BD, 4 | SYN_BD},
+	{"roff", {4, 0, 5, 4, 5, 4},
 		"^[.'][ \t]*((SH.*)|(de) (.*)|([^ \t\\]{2,}))?.*$", 1},
-	{"roff", {2 | SYN_IT}, "\\\\\".*$"},
+	{"roff", {2}, "\\\\\".*$"},
 	{"roff", {3}, "\\\\{1,2}[*$fgkmns]([^[(]|\\(..|\\[[^]]*\\])"},
 	{"roff", {3}, "\\\\([^[(*$fgkmns]|\\(..|\\[[^]]*\\])"},
 	{"roff", {3}, "\\$[^$]+\\$"},
 
-	{"tex", {4 | SYN_BD, 0, 3, 0, 5},
+	{"tex", {4, 0, 3, 0, 5},
 		"\\\\[^[{ \t]+(\\[([^]]+)\\])?(\\{([^}]*)\\})?"},
 	{"tex", {3}, "\\$[^$]+\\$"},
-	{"tex", {2 | SYN_IT}, "%.*$"},
+	{"tex", {2}, "%.*$"},
 
 	/* mail */
-	{"msg", {6 | SYN_BD}, "^From .*20..$"},
-	{"msg", {6 | SYN_BD, 4 | SYN_BD}, "^Subject: (.*)$"},
-	{"msg", {6 | SYN_BD, 2 | SYN_BD}, "^From: (.*)$"},
-	{"msg", {6 | SYN_BD, 5 | SYN_BD}, "^To: (.*)$"},
-	{"msg", {6 | SYN_BD, 5 | SYN_BD}, "^Cc: (.*)$"},
-	{"msg", {6 | SYN_BD}, "^[A-Z][-A-Za-z]+: .+$"},
-	{"msg", {2 | SYN_IT}, "^> .*$"},
+	{"msg", {6}, "^From .*20..$"},
+	{"msg", {6, 4}, "^Subject: (.*)$"},
+	{"msg", {6, 2}, "^From: (.*)$"},
+	{"msg", {6, 5}, "^To: (.*)$"},
+	{"msg", {6, 5}, "^Cc: (.*)$"},
+	{"msg", {6}, "^[A-Z][-A-Za-z]+: .+$"},
+	{"msg", {2}, "^> .*$"},
 
 	/* makefile */
 	{"mk", {0, 3}, "([A-Za-z_][A-Za-z0-9_]*)[ \t]*="},
 	{"mk", {3}, "\\$\\([a-zA-Z0-9_]+\\)"},
-	{"mk", {2 | SYN_IT}, "#.*$"},
-	{"mk", {0, SYN_BD}, "([A-Za-z_%.]+):"},
+	{"mk", {2}, "#.*$"},
+	{"mk", {0}, "([A-Za-z_%.]+):"},
 
 	/* shell script */
-	{"sh", {5 | SYN_BD}, "\\<(break|case|continue|do|done|elif|else|esac|fi|for|if|in|then|until|while|return)\\>"},
+	{"sh", {5}, "\\<(break|case|continue|do|done|elif|else|esac|fi|for|if|in|then|until|while|return)\\>"},
 	{"sh", {4}, "\"([^\"\\]|\\\\.)*\""},
 	{"sh", {4}, "'[^']*'"},
 	{"sh", {3}, "`([^`\\]|\\\\.)*`"},
 	{"sh", {1}, "\\$(\\{[^}]+\\}|[a-zA-Z_0-9]+|[!#$?*@-])"},
-	{"sh", {0, SYN_BD}, "^([a-zA-Z_][a-zA-Z_0-9]* *\\(\\)) *\\{"},
+	{"sh", {0}, "^([a-zA-Z_][a-zA-Z_0-9]* *\\(\\)) *\\{"},
 	{"sh", {SYN_BD}, "^\\. .*$"},
-	{"sh", {2 | SYN_IT}, "#.*$"},
+	{"sh", {2}, "#.*$"},
 
-	{"bib", {0, 8 | SYN_BD, SYN_BGMK(11) | SYN_BD}, "^(%L) +(.*)$", 1},
-	{"bib", {0, 8 | SYN_BD, 12 | SYN_BD}, "^(%A) (.*)$", 1},
-	{"bib", {0, 8 | SYN_BD, 5 | SYN_BD}, "^(%T) (.*)$", 1},
-	{"bib", {0, 8 | SYN_BD, 2 | SYN_BD}, "^(%[JB]) (.*)$", 1},
-	{"bib", {0, 8 | SYN_BD, 5 | SYN_BD}, "^(%D) (.*)$", 1},
-	{"bib", {0, 8 | SYN_BD, 7}, "^(%O) (.*)$", 1},
-	{"bib", {0, 8 | SYN_BD, 8 | SYN_BD}, "^(%[A-Z]) (.*)$", 1},
+	{"bib", {0, 8, SYN_BGMK(11)}, "^(%L) +(.*)$", 1},
+	{"bib", {0, 8, 12}, "^(%A) (.*)$", 1},
+	{"bib", {0, 8, 5}, "^(%T) (.*)$", 1},
+	{"bib", {0, 8, 2}, "^(%[JB]) (.*)$", 1},
+	{"bib", {0, 8, 5}, "^(%D) (.*)$", 1},
+	{"bib", {0, 8, 7}, "^(%O) (.*)$", 1},
+	{"bib", {0, 8, 8}, "^(%[A-Z]) (.*)$", 1},
 	{"bib", {25}, "^#.*$", 1},
 
 	/* python */
@@ -91,18 +103,18 @@ static struct highlight {
 	{"py", {5}, "\\<(import|from|global|lambda|del)\\>"},
 	{"py", {5}, "\\<(for|while|if|elif|else|pass|return|break|continue)\\>"},
 	{"py", {5}, "\\<(try|except|as|raise|finally|with)\\>"},
-	{"py", {0, 0 | SYN_BD}, "([a-zA-Z][a-zA-Z0-9_]+)\\(", 1},
+	{"py", {0, 0}, "([a-zA-Z][a-zA-Z0-9_]+)\\(", 1},
 	{"py", {4}, "[\"']([^\"']|\\\\\")*[\"']"},
 
 	/* neatmail */
-	{"nm", {0 | SYN_BGMK(15), 6 | SYN_BD, 12 | SYN_BD, 5, 8 | SYN_BD},
+	{"nm", {0 | SYN_BGMK(15), 6, 12, 5, 8},
 		"^([ROU])([0-9]+)\t([^\t]*)\t([^\t]*)"},
-	{"nm", {0 | SYN_BD | SYN_BGMK(6)}, "^[N].*$"},
-	{"nm", {0 | SYN_BD | SYN_BGMK(13)}, "^[A-Z][HT].*$"},
-	{"nm", {0 | SYN_BD | SYN_BGMK(11)}, "^[A-Z][MI].*$"},
+	{"nm", {0 | SYN_BGMK(6)}, "^[N].*$"},
+	{"nm", {0 | SYN_BGMK(13)}, "^[A-Z][HT].*$"},
+	{"nm", {0 | SYN_BGMK(11)}, "^[A-Z][MI].*$"},
 	{"nm", {7 | SYN_BGMK(15)}, "^[A-Z][LJ].*$"},
-	{"nm", {0 | SYN_BD | SYN_BGMK(7)}, "^[F].*$"},
-	{"nm", {7 | SYN_IT}, "^\t.*$"},
+	{"nm", {0 | SYN_BGMK(7)}, "^[F].*$"},
+	{"nm", {7}, "^\t.*$"},
 	{"nm", {SYN_BD}, "^:.*$"},
 
 	/* diff */
@@ -112,9 +124,33 @@ static struct highlight {
 	{"diff", {SYN_BD}, "^diff .*$"},
 
 	/* status bar */
-	{"---", {8 | SYN_BD, 4, 1}, "^(\".*\").*(\\[[wr]\\]).*$"},
-	{"---", {8 | SYN_BD, 4, 4}, "^(\".*\").*(L[0-9]+) +(C[0-9]+).*$"},
-	{"---", {8 | SYN_BD}, "^.*$"},
+	{"---", {8, 4, 1}, "^(\".*\").*(\\[[wr]\\]).*$"},
+	{"---", {8, 4, 4}, "^(\".*\").*(L[0-9]+) +(C[0-9]+).*$"},
+	{"---", {8}, "^.*$"},
+
+        /* rust */
+	{"rust", {HL_TYPE}, "\\<(i|u|f)(8|16|32|64|128|size)\\>"},
+	{"rust", {HL_TYPE}, "(->|:) (&[a-zA-Z']* |[a-zA-Z&])[a-zA-Z0-9_: ]*"},
+	{"rust", {HL_TYPE}, "<[a-zA-Z&'][a-zA-Z0-9_:, '+]*>"},
+	{"rust", {HL_KEYWORD}, "\\<(ref|const|impl|self|pub|struct|enum|as|move|static|super|trait|type|unsafe|where|async|await|dyn|use|crate|let|fn|return|for|in|while|loop|if|else|match|break|continue|mut|macro_rules)\\>"},
+	{"rust", {HL_COMMENT}, "//.*$"},
+	{"rust", {HL_FUNCTION}, "([a-zA-Z][a-zA-Z0-9_]+)\\(", 1},
+	{"rust", {HL_FUNCTION}, " (==|!=|<|>|%|-=|+=|=>|<=|>=) "},
+	{"rust", {HL_STRING}, "\"(\\\\\"|[^\"])*\""},
+	{"rust", {HL_STRING}, "b?'(\\\\'|[^'])*'"},
+	{"rust", {HL_NUMBER}, "[-+]?\\<(0[xbo][0-9a-fA-F_]+|[0-9_]+)\\>"},
+
+	/* smalltalk */
+	{"st", {HL_TYPE}, "\\|.*\\|"},
+	{"st", {HL_TYPE}, "(^| )[A-Z][a-zA-Z0-9]*"},
+	{"st", {HL_FUNCTION}, "\\<(do|whileTrue|whileFalse|ifTrue|ifFalse|put|to|at|add|new|for|super|subclass|class|init|category)\\>:?"},
+	{"st", {HL_FUNCTION}, " (:=|>>) "},
+	{"st", {HL_FUNCTION}, "\\^"},
+	{"st", {HL_COMMENT}, "\"[^\"]*\""},
+	{"st", {HL_MISC}, "#([a-zA-Z_][a-zA-Z0-9_]*|'[^']*')"},
+	{"st", {HL_STRING}, "'[^']*'"},
+	{"st", {HL_STRING}, "\\$.?"},
+	{"st", {HL_NUMBER}, "[-+]?\\<([0-9]+r[0-9a-fA-F]+|[0-9]+)\\>"}
 };
 
 /* how to hightlight current line (hll option) */
